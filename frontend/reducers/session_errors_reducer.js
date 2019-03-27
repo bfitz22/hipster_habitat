ce6f5ee262246dfa6149b513f36f9e9a95a57d91
’bfitz22’ <bffitzgerald89@gmail.com>
2019-03-27 19:43:19 -0400

    begin css on user auth

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,11 +1,13 @@
 import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from '../actions/session_actions';
+import { CLOSE_MODAL } from '../actions/modal_actions';
 
 const sessionErrorsReducer = (state = [], action) => {
     Object.freeze(state);
     switch(action.type){
         case RECEIVE_ERRORS: 
-            return [].concat(state).concat(action.errors);
+            return action.errors;
         case RECEIVE_CURRENT_USER:
+        case CLOSE_MODAL:
             return [];
         default: 
             return state;

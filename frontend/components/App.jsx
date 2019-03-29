82ba5b510c88500837e9df2250f7a699b0b12700
’bfitz22’ <bffitzgerald89@gmail.com>
2019-03-29 13:40:39 -0400

    upload for project review

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import Nav from './nav';
 import Footer from './footer';
+import Autocomplete from './autocomplete';
+
+const names = [
+    "pet friendly",
+    "campfires",
+    "water on site",
+    "toilets on site",
+    "showers on site",
+    "wifi available",
+    "large group",
+    "hiking",
+    "biking",
+    "swimming",
+    "fishing",
+    "horseback riding",
+    "climbing"
+];
 
 const App = () => (
     <body className="body">
         <Nav />
+        <Autocomplete names={names} />
         <Footer />
     </body>
 );
--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -9,7 +9,17 @@ class Footer extends React.Component {
                     <a href="https://www.linkedin.com/in/brian-fitzgerald-643a6757/"><i className="fab fa-linkedin"></i></a>
                     <a href="https://www.hipcamp.com/"><i className="fas fa-campground"></i></a>
                 </div>
-                <img className="footer-img" src={window.footerURL} />
+                <div className="footer-body">
+                    <section className="footer-title">
+                        HipsterHabitat is everywhere you want to camp.
+                    </section>
+                    <section className="footer-text">
+                        Discover unique experiences on ranches, nature preserves, farms, vineyards, and public
+                        campgrounds across the U.S. Book tent camping, treehouses, cabins, yurts, primitive
+                        backcountry sites, car camping, airstreams, tiny houses, RV camping, glamping tents
+                        and more. 
+                    </section>
+                </div>
             </footer>
         )
     }

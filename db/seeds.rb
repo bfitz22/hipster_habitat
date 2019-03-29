9a624a1047526d25ee89ccf041181940f1514314
’bfitz22’ <bffitzgerald89@gmail.com>
2019-03-29 10:23:43 -0400

    add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+User.destroy_all
 demo = User.create({
     password: "123456",
     first_name: "Yogi",

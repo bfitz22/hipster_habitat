75d075300f345d56f95404d58e9dfdbbc8bf4a09
’bfitz22’ <bffitzgerald89@gmail.com>
2019-04-03 15:23:39 -0400

    add new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,9 +45,15 @@ hope = Listing.create!({
 file = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_hope.jpg')
 file3 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_hope1.jpg')
 file4 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_hope2.jpg')
+file9 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_hope3.JPG')
+file10 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_hope4.jpg')
+file11 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_hope5.jpg')
 hope.photos.attach(io: file, filename: 'camp_hope.jpg')
 hope.photos.attach(io: file3, filename: 'camp_hope1.jpg')
 hope.photos.attach(io: file4, filename: 'camp_hope2.jpg')
+hope.photos.attach(io: file9, filename: 'camp_hope3.jpg')
+hope.photos.attach(io: file10, filename: 'camp_hope4.jpg')
+hope.photos.attach(io: file11, filename: 'camp_hope5.jpg')
 
 firewood = Listing.create!({
     host: user,
@@ -70,9 +76,15 @@ firewood = Listing.create!({
 file1 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_firewood.jpg')
 file5 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_firewood1.jpg')
 file6 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_firewood2.jpg')
+file12 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_firewood3.jpg')
+file13 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_firewood4.jpg')
+file14 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_firewood5.jpg')
 firewood.photos.attach(io: file1, filename: 'camp_firewood.jpg')
 firewood.photos.attach(io: file5, filename: 'camp_firewood1.jpg')
-firewood.photos.attach(io: file6, filename: 'camp_firewood.jpg')
+firewood.photos.attach(io: file6, filename: 'camp_firewood2.jpg')
+firewood.photos.attach(io: file12, filename: 'camp_firewood3.jpg')
+firewood.photos.attach(io: file13, filename: 'camp_firewood4.jpg')
+firewood.photos.attach(io: file14, filename: 'camp_firewood5.jpg')
 
 crystal_lake = Listing.create!({
     host: user,
@@ -96,6 +108,39 @@ crystal_lake = Listing.create!({
 file2 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_crystal_lake.jpg')
 file7 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_crystal_lake1.jpg')
 file8 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_crystal_lake2.jpg')
+file15 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_crystal_lake3.jpg')
+file16 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_crystal_lake4.jpg')
+file17 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_crystal_lake5.jpg')
 crystal_lake.photos.attach(io: file2, filename: 'camp_crystal_lake.jpg')
 crystal_lake.photos.attach(io: file7, filename: 'camp_crystal_lake1.jpg')
 crystal_lake.photos.attach(io: file8, filename: 'camp_crystal_lake2.jpg')
+crystal_lake.photos.attach(io: file15, filename: 'camp_crystal_lake3.jpg')
+crystal_lake.photos.attach(io: file16, filename: 'camp_crystal_lake4.jpg')
+crystal_lake.photos.attach(io: file17, filename: 'camp_crystal_lake5.jpg')
+
+north_star = Listing.create!({
+    host: user,
+    title: "Camp Crystal Lake",
+    description: "Practical joker? You'll love this goofball-friendly environment.
+    Come check out this Canadian-inspired camp maintained by the legendary Tripper
+    Harrison. Lots of fun competition awaits with the many activities available.",
+    price: 18,
+    pets_allowed: false,
+    campfires_allowed: true, 
+    is_water: true,
+    is_toilets: true,
+    is_showers: false,
+    is_wifi: false,
+    max_capacity: 25,
+    lat: 40.745739,
+    lng: -73.843275,
+    location: "Queens, New York"
+})
+file18 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_white_pine.jpg')
+file19 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_white_pine1.jpg')
+file20 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_white_pine2.jpg')
+file21 = open('https://s3.amazonaws.com/hipsterhabitat-dev/camp_white_pine3.jpg')
+north_star.photos.attach(io: file18, filename: 'camp_north_star5.jpg')
+north_star.photos.attach(io: file19, filename: 'camp_north_star5.jpg')
+north_star.photos.attach(io: file20, filename: 'camp_north_star5.jpg')
+north_star.photos.attach(io: file21, filename: 'camp_north_star5.jpg')
--- frontend/components/listing/listing_index_item.jsx
+++ frontend/components/listing/listing_index_item.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const ListingIndexItem = ( props ) => {
     return (
-    <div className="listing-index-item">
-        <Link to={`/listings/${props.listing.id}`}>
-            {/* <img src={`${props.listing.listing_photos[0]}`}/> */}
-            <span>{props.listing.title}</span>
-            <span>${props.listing.price}/night</span>
-        </Link>
-    </div>
+    <div className="choice-div">
+      <a href={`/#/listings/${props.listing.id}`} className="option-links">
+        <img className="choice-pic" src={props.listing.listing_photos[0]} />
+        <p className="option-title">{props.listing.title}</p>
+        <p className="option-location"> {props.listing.location}</p>
+      </a>
+    </div> 
     )
 };
 

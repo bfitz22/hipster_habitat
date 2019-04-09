a33bc834aa4505086c67879c8c40dbdc6ab9d103
’bfitz22’ <bffitzgerald89@gmail.com>
2019-04-08 20:51:05 -0400

    begin listings create

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import Nav from './nav';
 import Footer from './footer';
 import ListingIndexContainer from '../components/listing/listing_index_container';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import MainMenuContainer from './main_menu/main_menu_container';
 import ListingDetailContainer from './listing/listing_detail_container';
+import ListingForm from './listing/listing_create/listing_form';
 
 const App = () => (
-        <div className="body">   
-            <Nav />
+        <div className="body"> 
+            <Route path="/listingcreate" component={ListingForm}/>     
+            <Route exact path="/" component={Nav}/>
+            <Route path="/discover" component={Nav}/>
+            <Route path="/listings/:listingId" component={Nav}/>
             <Route exact path="/" component={MainMenuContainer} />
             <Route path="/discover" component={ListingIndexContainer}/>
             <Route path="/listings/:listingId" component={ListingDetailContainer}/>
---
+++ frontend/components/listing/listing_create/listing_form.jsx
@@ -0,0 +1,14 @@
+export const ListingForm = () => {
+    return (
+        <div className="form-body">
+            <div className="form-title">
+                <h2>What is the Title of your Listing?</h2>
+                <input type="text"/>
+            </div>
+            <div>
+                <h2>Describe your Listing</h2>
+                <input type="textarea"/>
+            </div>
+        </div>
+    )
+} 

a0b24b9b4ee964c26d37ba5a9f9d88f3786fa7a5
bfitz22 <bffitzgerald89@gmail.com>
2019-10-10 08:33:29 -0400

    finish state in main_menu component

--- frontend/components/main_menu/main_menu.jsx
+++ frontend/components/main_menu/main_menu.jsx
@@ -12,38 +12,51 @@ class MainMenu extends React.Component {
                 {title: "Campsites", site: "tent", active: false, image: "fas fa-campground"},
                 {title: "Lodging", site: "cabin", active: false, image: "fas fa-home"},
                 {title: "RVs", site: "rv", active: false, image: "fas fa-shuttle-van"}
-            ], 
-            filteredListings: this.props.listings
+            ]
         }
         this.sliceListings = this.sliceListings.bind(this);
+        this.applyFilters = this.applyFilters.bind(this);
     }
+
+    // MainMenu(filteredListings) >> Search(searchListings) >> SearchModal
 
     componentDidMount() {
         this.props.fetchListings();
-        this.setState({ arr: this.state.arr, filteredListings: this.props.listings })
+        this.setState({ arr: this.state.arr })
     }
 
     toggle(index) {
         let arr = this.state.arr;
         arr[index].active = !arr[index].active;
         this.setState({ arr: arr });
+        this.applyFilters();
     }
 
     applyFilters() {
-        
+        let listings = [];
+        this.state.arr.forEach((el) => {
+            if (el.active) {
+                this.props.listings.forEach((listing) => {
+                    if (el.site === listing.site) {
+                        listings.push(listing)
+                    }
+                })
+            }
+        })
+        this.setState({ filteredListings: listings })
     }
 
     sliceListings() {
         return this.props.listings.slice(0, 3);
     }
 
-    searchIdeas(query) {
-        let listings = this.props.listings.filter((listing) => {
-            return listing.title.includes(query) ||
-            listing.description.includes(query)
-        });
-        console.log(listings)
-    }
+    // searchIdeas(query) {
+    //     let listings = this.props.listings.filter((listing) => {
+    //         return listing.title.toLowerCase().includes(query.toLowerCase()) ||
+    //         listing.description.toLowerCase().includes(query.toLowerCase())
+    //     });
+    //     this.setState({ searchListings: listings })
+    // }
 
     render() {
         const options = this.state.arr.map((el, i) => 

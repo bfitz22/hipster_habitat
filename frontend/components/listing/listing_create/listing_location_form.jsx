503b8a9b500d9fc1e146fc8ae4d5294e4b27c1a6
bfitz22 <bffitzgerald89@gmail.com>
2019-12-11 22:19:02 -0600

    update form to take in full address

--- frontend/components/listing/listing_create/listing_location_form.jsx
+++ frontend/components/listing/listing_create/listing_location_form.jsx
@@ -9,11 +9,15 @@ import { connect } from 'react-redux';
 class ListingLocationForm extends React.Component {
     constructor(props) {
         super(props);
-        // const lat = this.props.lat || "";
-        // const lng = this.props.lng || "";
-        const city = this.props.location || "";
+        const street = this.props.street || "";
+        const city = this.props.location.split(", ")[0] || "";
+        const USstate = this.props.location.split(", ")[1] || "";
+        const zip = this.props.zip || "";
         this.state = { 
+            street,
             city,
+            USstate,
+            zip,
             errors: [] };
         this.onClick = this.onClick.bind(this);
         this.geocode = Geocode;
@@ -24,11 +28,12 @@ class ListingLocationForm extends React.Component {
         let address = this.state.street + " " + this.state.city + " " + this.state.USstate + " " + this.state.zip;
         this.geocode.fromAddress(address).then(
             response => {
-                debugger
                 const { lat, lng } = response.results[0].geometry.location;
                 this.props.updateCreation("lat", lat)
                 this.props.updateCreation("lng", lng)
                 this.props.updateCreation("location", this.state.city + ", " + this.state.USstate)
+                this.props.updateCreation("street", this.state.street)
+                this.props.updateCreation("zip", this.state.zip)
             },
             error => {
                 console.log(error);
@@ -44,20 +49,37 @@ class ListingLocationForm extends React.Component {
     }
 
     render() {
+        const states = ["----", "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", 
+        "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", 
+        "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", 
+        "VA", "WA", "WV", "WI", "WY"]
+        
+        let currentState = this.state.USstate.toString();
+        const options = states.map((el, i) => {
+            if (currentState === el) {
+                return <option selected key={i} value={el} defaultValue={el}>{el}</option>
+            } else {
+                return <option key={i} value={el}>{el}</option>
+            }
+        })
+
         let ok;
-        if (this.state.lat === "" || this.state.lng === "" || this.state.location === "") {
+        if (this.state.street === "" || this.state.city === "" || this.state.USstate === "----" || this.state.zip === "") {
             ok = <button className="not-ok">Ok</button>
         } else {
             ok = <button className="ok" onClick={this.onClick}>Ok</button>
         }
+
+        let next = null;
+        if (this.props.lat) { next = "/#/listing_create/price"}
 
         return (
             <>
             <ListingNav />
             <div className="form-body">
                 <div className="nav-arrow-container">
-                    <NavLeft/>
-                    <NavRight/>
+                    <NavLeft prev={"/#/listing_create/"}/>
+                    <NavRight next={next}/>
                 </div>
                 <div className="form-vessel">
                     <div className="form-container">
@@ -66,13 +88,13 @@ class ListingLocationForm extends React.Component {
                         </div>
                         <div className="form-input">
                             <p>Street Address</p>
-                            <input type="text" value={this.state.street} onChange={this.update("street")}/>
+                            <input type="text" value={this.state.street} placeholder={this.state.street} onChange={this.update("street")}/>
                             <p>City</p>
-                            <input type="text" value={this.state.city} onChange={this.update("city")} />
+                            <input type="text" value={this.state.city} placeholder={this.state.city} onChange={this.update("city")} />
                             <p>State</p>
-                            <input type="text" value={this.state.USstate} onChange={this.update("USstate")} />
+                            <select type="select" onChange={this.update("USstate")}>{options}</select> 
                             <p>Zip Code</p>
-                            <input type="text" value={this.state.zip} onChange={this.update("zip")} />
+                            <input type="text" value={this.state.zip} placeholder={this.state.zip} onChange={this.update("zip")} />
                         </div>
                         <div>
                             {ok}
@@ -99,7 +121,9 @@ const msp = ({ entities: { creations } }) => {
     return {
         lat: creations.lat,
         lng: creations.lng,
-        location: creations.location
+        location: creations.location, 
+        street: creations.street,
+        zip: creations.zip
     }
 }
 
